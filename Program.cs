a0f324715d576e3584de939725be8e884755a1c4
Laya Anil Kumar Veena <lanilkumarveena3253@conestogac.on.ca>
2025-01-09 11:17:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Detection
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+
+        public static string OrginalFilePath = "";
+        public static string sliceFilePath = "";
+        public static Bitmap blackImage;
+
+        public static string ss = "";
+
+
+        public static Bitmap org;
+        public static Bitmap tumour;
+        public static Bitmap HOG;
+        public static string text = "";
+
+        public static double sdata = 0.75;
+
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new BasePage());
+        }
+    }
+}
